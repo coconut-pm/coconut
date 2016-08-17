2feb46d935aa56573c50f8e841ce3a976298987d
Oskar Nyberg <oskar@oskarnyberg.com>
2016-08-17 21:58:25 +0200

    Added get function to cli client

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,17 @@
 #!/usr/bin/env node
 
-const program = require('commander');
+const db = require('../src/db'),
+      program = require('commander');
+
+program
+  .version('0.0.1');
+
+program
+  .command('get <entry>')
+  .description('Returns an entry by either it\'s number')
+  .action(entryId => {
+    console.log(db.getEntry(entryId));
+  });
+
+// vim: sw=2
 

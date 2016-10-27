174a4b8c338675692c805d16b552e545f5c6b669
Oskar Nyberg <oskar@oskarnyberg.com>
2016-10-27 17:49:12 +0200

    Refactored tests

--- test/db.js
+++ test/db.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect,
       DB = require('../src/db')
 
-describe('Communication with IPFS', function() {
+describe('Communication with orbit-db-feedstore', function() {
   let db
 
   before(function() {
@@ -35,29 +35,8 @@ describe('Communication with IPFS', function() {
       })
   })
 
-  it('should get object by hash', function(done) {
-    db.add({a: 'b'})
-      .then(() => {
-        let entry0 = db.entries[0]
-        let entry = db.get(entry0.hash)
-        expect(entry).to.deep.equal(entry0)
-        done()
-      })
-  })
-
   it('should return undefined if hash doesn\'t exists', function() {
     expect(db.get('nonexistenthash')).to.be.undefined
-  })
-
-  it('should remove an object', function(done) {
-    db.add({a: 'b'})
-      .then(() => {
-        let entry0 = db.entries[0]
-        return db.remove(entry0.hash)
-      }).then(() => {
-        expect(db.entries).to.be.empty
-        done()
-      })
   })
 
   it('should reject when trying to delete non-existent hash', function(done) {
@@ -67,33 +46,9 @@ describe('Communication with IPFS', function() {
       })
   })
 
-  it('should update an object', function(done) {
-    let newEntry = {a: 5, b: 'c'}
-    db.add({a: 'b'})
-      .then(() => {
-        let entry0 = db.entries[0]
-        return db.update(entry0.hash, newEntry)
-      }).then(() => {
-        expect(db.entries[0].value).to.deep.equal(newEntry)
-        done()
-      })
-  })
-
   it('should reject when trying to update non-existent hash', function(done) {
-    db.remove('nonexistenthash', {test: 'test'})
+    db.update('nonexistenthash', {test: 'test'})
       .catch(() => {
-        done()
-      })
-  })
-
-  it('should sync from hash', function(done) {
-    let db2
-    db.add({a: 'b'})
-      .then(() => {
-        db2 = new DB('testuser', 'testpassword')
-        return db2.sync(db.hash)
-      }).then(() => {
-        expect(db2.entries).to.deep.equal(db.entries)
         done()
       })
   })
@@ -103,6 +58,49 @@ describe('Communication with IPFS', function() {
       .catch(() => {
         done()
       })
+  })
+
+  describe('operating on an existing object', function() {
+    beforeEach(function(done) {
+      db.add({a: 'b'})
+        .then(() => {
+          done()
+        })
+    })
+
+    it('should get object by hash', function() {
+      let entry0 = db.entries[0]
+      let entry = db.get(entry0.hash)
+      expect(entry).to.deep.equal(entry0)
+    })
+
+    it('should remove an object', function(done) {
+      let entry0 = db.entries[0]
+      db.remove(entry0.hash)
+        .then(() => {
+          expect(db.entries).to.be.empty
+          done()
+        })
+    })
+
+    it('should update an object', function(done) {
+      let newEntry = {a: 5, b: 'c'}
+      let entry0 = db.entries[0]
+      db.update(entry0.hash, newEntry)
+        .then(() => {
+          expect(db.entries[0].value).to.deep.equal(newEntry)
+          done()
+        })
+    })
+
+    it('should sync from hash', function(done) {
+      let db2 = new DB('testuser', 'testpassword')
+      db2.sync(db.hash)
+        .then(() => {
+          expect(db2.entries).to.deep.equal(db.entries)
+          done()
+        })
+    })
   })
 })
 

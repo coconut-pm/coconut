1a0e83a2c9d7d4edc6eea79d4d82112fe2efca60
Oskar Nyberg <oskar@oskarnyberg.com>
2016-10-27 11:14:12 +0200

    Added missing references to this

--- src/db.js
+++ src/db.js
@@ -1,16 +1,15 @@
 const IPFS = require('ipfs-api'),
       FeedStore = require('orbit-db-feedstore'),
-      utils = require('./utils');
-
-const enc = require('./encryption.js')
+      utils = require('./utils'),
+      Encryption = require('./encryption.js');
 
 const DB_NAME = 'coconut';
 
 class DB {
   constructor(id = mandatory(), password = mandatory()) {
-    this.key = enc.expandKey(password);
-    this.ipfs = new IPFS();
-    this.store = new FeedStore(this.ipfs, id, DB_NAME);
+    this.key = Encryption.expandKey(password);
+    let ipfs = new IPFS();
+    this.store = new FeedStore(ipfs, id, DB_NAME);
 
     this._registerListeners();
   }
@@ -27,7 +26,7 @@ class DB {
   }
 
   add(entry = mandatory()) {
-    let encEntry = enc.encryptJson(entry, key)
+    let encEntry = Encryption.encryptJson(entry, this.key)
     return this.store.add(encEntry);
   }
 
@@ -45,7 +44,7 @@ class DB {
       .collect()
       .map(e => {
         let encEntry = e.payload.value
-        return enc.decryptJson(encEntry.ciphertext, encEntry.nonce, key)
+        return Encryption.decryptJson(encEntry.ciphertext, encEntry.nonce, this.key)
       })
   }
 
@@ -53,6 +52,16 @@ class DB {
     return this._hash;
   }
 }
+
+let db = new DB('oskar', 'asdf');
+db.sync('QmbTPSzyHEM64VkC6w9LcEDaXRMi3LcprqALYczck4MiNY')
+  .then(() => {
+    console.log(db.entries);
+  });
+/*db.add({a: 'b'})
+  .then(() => {
+    console.log(db.hash);
+  });*/
 
 module.exports = DB;
 

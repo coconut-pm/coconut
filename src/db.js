e652daff40c9f7ad74ca9d5adf6668d0d4a7b004
Oskar Nyberg <oskar@oskarnyberg.com>
2016-10-27 11:26:49 +0200

    Now returning hash with entry to make it possible to update or delete an entry

--- src/db.js
+++ src/db.js
@@ -1,69 +1,74 @@
 const IPFS = require('ipfs-api'),
       FeedStore = require('orbit-db-feedstore'),
       utils = require('./utils'),
-      Encryption = require('./encryption.js');
+      Encryption = require('./encryption.js')
 
-const DB_NAME = 'coconut';
+const DB_NAME = 'coconut'
 
 class DB {
   constructor(id = mandatory(), password = mandatory()) {
-    this.key = Encryption.expandKey(password);
-    let ipfs = new IPFS();
-    this.store = new FeedStore(ipfs, id, DB_NAME);
+    this.key = Encryption.expandKey(password)
+    let ipfs = new IPFS()
+    this.store = new FeedStore(ipfs, id, DB_NAME)
 
-    this._registerListeners();
+    this._registerListeners()
   }
 
   _registerListeners() {
     this.store.events.on('write', (dbname, hash) => {
-      this._hash = hash;
-    });
+      this._hash = hash
+    })
   }
 
   sync(hash = mandatory()) {
-    this._hash = hash;
-    return this.store.sync(hash);
+    this._hash = hash
+    return this.store.sync(hash)
   }
 
   add(entry = mandatory()) {
     let encEntry = Encryption.encryptJson(entry, this.key)
-    return this.store.add(encEntry);
+    return this.store.add(encEntry)
   }
 
   remove(hash = mandatory()) {
-    return this.store.remove(hash);
+    return this.store.remove(hash)
   }
 
   update(hash = mandatory(), entry = mandatory()) {
     return remove(hash)
-      .then(this.add.bind(this, entry));
+      .then(this.add.bind(this, entry))
+  }
+
+  get(hash = mandatory()) {
+    let e = this.store.get(hash)
+    return {
+      hash: e.hash,
+      value: this._decrypt(e)
+    }
   }
 
   get entries() {
     return this.store.iterator({ limit: -1 })
       .collect()
       .map(e => {
-        let encEntry = e.payload.value
-        return Encryption.decryptJson(encEntry.ciphertext, encEntry.nonce, this.key)
+        return {
+          hash: e.hash,
+          value: this._decrypt(e)
+        }
       })
   }
 
+  _decrypt(e) {
+    let encEntry = e.payload.value
+    return Encryption.decryptJson(encEntry.ciphertext, encEntry.nonce, this.key)
+  }
+
   get hash() {
-    return this._hash;
+    return this._hash
   }
 }
 
-let db = new DB('oskar', 'asdf');
-db.sync('QmbTPSzyHEM64VkC6w9LcEDaXRMi3LcprqALYczck4MiNY')
-  .then(() => {
-    console.log(db.entries);
-  });
-/*db.add({a: 'b'})
-  .then(() => {
-    console.log(db.hash);
-  });*/
-
-module.exports = DB;
+module.exports = DB
 
 // vim: sw=2
 

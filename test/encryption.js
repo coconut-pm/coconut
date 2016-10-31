d8064dbf8e25cbb568460073c869c57a12dba0e9
Joel Torstensson <oed3933@gmail.com>
2016-10-31 09:48:47 +0100

    Added tests for encryption

---
+++ test/encryption.js
@@ -0,0 +1,25 @@
+const expect    = require('chai').expect
+const enc       = require('../src/encryption')
+
+describe('Encryption functions', () => {
+  let key
+
+  it('should expand a key correctly', (done) => {
+    let password = 'mysecurepassword'
+    key = enc.expandKey(password)
+    expect(key.length).to.equal(32)
+    done()
+  })
+
+  it('should encrypt and decrypt correctly', (done) => {
+    let jsonData = {
+      wow: 'much data',
+      very: 'wow'
+    }
+    let encrypted = enc.encryptJson(jsonData, key)
+    let decrypted = enc.decryptJson(encrypted.ciphertext, encrypted.nonce, key)
+    expect(decrypted.very).to.equal(jsonData.very)
+    expect(decrypted.wow).to.equal(jsonData.wow)
+    done()
+  })
+})

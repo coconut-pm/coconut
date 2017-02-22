21074b91f826443a8baf721a9e72845447d2041e
Oskar Nyberg <oskar@oskarnyberg.com>
2017-02-22 21:15:30 +0100

    Added prompt dependency

--- bin/cli.js
+++ bin/cli.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 
 const DB = require('../src/db'),
-      program = require('commander');
+      program = require('commander'),
+      prompt = require('prompt')
 
 program
-  .version('0.0.1');
+  .version('0.0.1')
 
 program
   .command('get <entry>')
   .description('Returns an entry by either it\'s number')
   .action(entryId => {
-    console.log(db.getEntry(entryId));
+    console.log(db.getEntry(entryId))
   });
 
 // vim: sw=2

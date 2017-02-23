9dadbf6c8f2e7414c1b9ede6e670c5ee6e7b3be8
Joel Torstensson <oed3933@gmail.com>
2017-02-23 22:40:09 +0100

    Only password on correct occations

--- bin/cli.js
+++ bin/cli.js
@@ -11,33 +11,32 @@ const HASH_FILE = os.homedir() + '/.local/share/coconut'
 
 prompt.message = ''
 
-let coconut
-fs.readFile(HASH_FILE, (error, data) => {
-  if (error) {
-    //program.parse(process.argv)
-    console.log('Coconut is not initialized. \nPlease run \'coconut init\'')
-    process.exit()
-  } else {
-    mainPrompt(data.toString().trim())
-  }
-})
+//let coconut
 
-function mainPrompt(hash) {
-  prompt.get({
-      properties: {
-        masterPassword: {
-          hidden: true,
-          message: 'Master password'
-        },
-      }
-    }, (error, result) => {
-      coconut = new Coconut(result.masterPassword)
-      coconut.connect(hash).then(() => {
-        program.parse(process.argv)
-      }).catch((err) => {
-        console.log(err.message)
-      })
-    })
+function openDB(callback) {
+  fs.readFile(HASH_FILE, (error, data) => {
+    if (error) {
+      //program.parse(process.argv)
+      console.log('Coconut is not initialized. \nPlease run \'coconut init\'')
+      process.exit()
+    } else {
+      prompt.get({
+          properties: {
+            masterPassword: {
+              hidden: true,
+              message: 'Master password'
+            },
+          }
+        }, (error, result) => {
+          let coconut = new Coconut(result.masterPassword)
+          coconut.connect(data.toString().trim()).then(() => {
+            callback(coconut)
+          }).catch((err) => {
+            console.log(err.message)
+          })
+        })
+    }
+  })
 }
 
 function writeHash(hash, callback) {
@@ -51,24 +50,26 @@ program
   .command('add')
   .description('Add an entry')
   .action(() => {
-    prompt.get({
-      properties: {
-        service: {
-          required: true
-        },
-        username: {
-          require: true
-        },
-        password: {
-          hidden: true,
-        },
-        url: {},
-        notes: {}
-      }
-    }, (err, result) => {
-      coconut.addEntry(result.service, result.username, result.password,
-          result.url, result.notes).then(() => {
-        writeHash(coconut.hash, console.log.bind(console))
+    openDB((coconut) => {
+      prompt.get({
+        properties: {
+          service: {
+            required: true
+          },
+          username: {
+            require: true
+          },
+          password: {
+            hidden: true,
+          },
+          url: {},
+          notes: {}
+        }
+      }, (err, result) => {
+        coconut.addEntry(result.service, result.username, result.password,
+            result.url, result.notes).then(() => {
+          writeHash(coconut.hash, console.log.bind(console))
+        })
       })
     })
   });
@@ -83,15 +84,21 @@ program
   .command('list')
   .description('List of all entries')
   .action(() => {
-    console.log(coconut.entries)
+    openDB((coconut) => {
+      console.log(coconut.entries)
+    })
   })
 
 program
   .command('search')
   .description('Search for entries')
   .action(() => {
-    console.log(coconut.search(process.argv[3]))
+    openDB((coconut) => {
+      console.log(coconut.search(process.argv[3]))
+    })
   })
+
+program.parse(process.argv)
 
 // vim: sw=2
 

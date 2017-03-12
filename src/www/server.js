fdf4085fcfc7f7a63f22f9051503cb0b6f8da40a
Oskar Nyberg <oskar@oskarnyberg.com>
2017-03-12 21:04:42 +0100

    Created html template

--- src/www/server.js
+++ src/www/server.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use('/modules', serveStatic(path.resolve('../../node_modules/')))
 app.use('/', serveStatic(path.resolve('static/')))
+app.use('/', serveStatic(path.resolve('resources/')))
 
 app.listen(8081, () => {
   console.log('Server running on port 8081.')

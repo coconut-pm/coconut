0228e6393a50f232493f4ce3d39011203a109263
Oskar Nyberg <oskar@oskarnyberg.com>
2017-03-13 21:25:10 +0100

    Made it possible to connect and list entries in web interface

--- src/www/static/script.js
+++ src/www/static/script.js
@@ -1,6 +1,41 @@
-window.onload = () => {
-    let coconut = new Coconut('hej')
-    console.log(coconut.entries)
+let coconut
+
+function passwordEntered(form) {
+  let password = form.password.value
+  coconut = new Coconut(password)
+
+  let hash = localStorage.getItem('hash')
+  if (hash) {
+    coconut.connect(hash)
+      .then(() => {
+        document.querySelector('#incorrectPassword').textContent = ''
+        list()
+      }).catch(() => {
+        document.querySelector('#incorrectPassword').textContent = 'Wrong password'
+      })
+  }
+
+  return false
+}
+
+function add(service, username, password, url, notes) {
+  if (!coconut) {
+    return;
+  }
+  coconut.addEntry(...arguments)
+    .then(() => {
+      localStorage.setItem('hash', coconut.hash)
+    })
+}
+
+function list() {
+  if (!coconut) {
+    return;
+  }
+  document.querySelector('ul').innerHTML =
+    `${coconut.entries.map(entry => {
+      return `<li>${entry.value.service}</li>`
+    }).join('\n')}`
 }
 
 // vim: sw=2

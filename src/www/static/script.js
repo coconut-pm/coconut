9b76be71f4360208b4c048b189ff13cb6fdf2327
Oskar Nyberg <oskar@oskarnyberg.com>
2017-03-14 22:41:32 +0100

    Implemented all functionality except servers in web-app

--- src/www/static/script.js
+++ src/www/static/script.js
@@ -1,4 +1,5 @@
 let coconut
+let modifyFunction
 
 function passwordEntered(form) {
   let password = form.password.value
@@ -18,24 +19,58 @@ function passwordEntered(form) {
   return false
 }
 
-function add(service, username, password, url, notes) {
-  if (!coconut) {
-    return;
-  }
-  coconut.addEntry(...arguments)
-    .then(() => {
-      localStorage.setItem('hash', coconut.hash)
-    })
+function updateHash() {
+  localStorage.setItem('hash', coconut.hash)
 }
 
 function list() {
-  if (!coconut) {
-    return;
-  }
   document.querySelector('ul').innerHTML =
-    `${coconut.entries.map(entry => {
-      return `<li>${entry.value.service}</li>`
-    }).join('\n')}`
+    `${coconut.entries.map(listItem).join('\n')}`
+}
+
+function listItem(entry) {
+  let title = `<a href="#" onclick="show(\'${entry.hash}\')">${entry.value.service}</a>`
+  let edit = `<a href="#" onclick="edit(\'${entry.hash}\')">✎</a>`
+  let remove = `<a href="#" onclick="remove(\'${entry.hash}\')">✖</a>`
+  return `<li>${title} ${edit} ${remove}</li>`
+}
+
+function add() {
+  modifyFunction = coconut.addEntry.bind(coconut)
+}
+
+function edit(hash) {
+  modifyFunction = coconut.updateEntry.bind(coconut, hash)
+}
+
+function remove(hash) {
+  coconut.remove(hash)
+    .then(modified)
+}
+
+function doModify() {
+  modifyFunction(
+    document.forms.modify.service.value,
+    document.forms.modify.username.value,
+    document.forms.modify.password.value,
+    document.forms.modify.url.value,
+    document.forms.modify.notes.value
+  ).then(modified)
+
+  return false
+}
+
+function modified() {
+  updateHash()
+  list()
+}
+
+function show(hash) {
+  let entry = coconut.get(hash).value
+  document.querySelector('#service').textContent = entry.service
+  document.querySelector('#username').textContent = entry.username
+  document.querySelector('#url').textContent = entry.url
+  document.querySelector('#notes').textContent = entry.notes
 }
 
 // vim: sw=2
